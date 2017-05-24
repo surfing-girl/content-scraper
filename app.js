22048ee856c3ba3c762292d9cfed8a2139c777f5
surfing-girl <joanna.demecka@gmail.com>
2017-05-24 14:12:11 +0100

    Log file function added and url format fixed

--- app.js
+++ app.js
@@ -1,48 +1,73 @@
 const osmosis = require('osmosis');
 const fs = require('fs');
-var json2csv = require('json2csv');
+const json2csv = require('json2csv');
 const http = require('http');
+const winston = require('winston')
 
 function ContentScraper() {
   this.url = 'http://www.shirts4mike.com/';
   this.savedData = [];
   this.currentDate = new Date();
-  this.scrapeDate = `${this.currentDate.getFullYear()}-${this.currentDate.getMonth()+1}-${this.currentDate.getDate()}`;
-  this.csvFields = ['Title', 'Color', 'Price', 'ImageURL', 'URL', 'Time'];
+  this.currentMonth = this.currentDate.getMonth()+1;
+  if (this.currentMonth < 10) {
+    this.currentMonth = `0${this.currentMonth}`
+  }
+  this.scrapeDate = `${this.currentDate.getFullYear()}-${this.currentMonth}-${this.currentDate.getDate()}`;
+  this.timeStamp = this.currentDate.toLocaleTimeString();
+  this.csvFields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
 }
 
 ContentScraper.prototype.printError = function (msg, url) {
+  const notFoundMsg = `Your page: '${url}' was not found`;
+  const offLineMsg = 'You are offline!';
   if (msg.indexOf('404') > -1) {
-      console.error(`Your page: '${url}' was not found`);
+    this.createLogFile(notFoundMsg);
   } else if (msg.indexOf('getaddrinfo') > -1) {
-      console.error('You are offline!');
+    this.createLogFile(offLineMsg);
   } else {
-    console.error(msg);
+    this.createLogFile(msg);
   }
-};
+}
 
-ContentScraper.prototype.createDataDirectory = function () {
-  fs.stat("./data", function (err, stats){
+ContentScraper.prototype.createDirectory = function (directoryNamePath) {
+  fs.stat(directoryNamePath, function (err, stats){
     if (err) {
       // Directory doesn't exist or something.
-      console.log('Folder doesn\'t exist, so I made the folder data');
-      return fs.mkdir("./data");
+      console.log(`Folder doesn't exist, so I made the folder "${directoryNamePath}"`);
+      return fs.mkdir(directoryNamePath);
     }
     if (!stats.isDirectory()) {
       // This isn't a directory!
       console.log("This isn't a directory!");
-    } else {
-      console.log('Directory "data" does exist!');
     }
   });
+}
+
+ContentScraper.prototype.createLogFile = function (errorMessage) {
+  this.createDirectory('./errors');
+  const logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        timestamp: this.timeStamp,
+        colorize: true,
+        level: 'error'
+      }),
+      new (winston.transports.File)({
+        filename: `./errors/scraper-error.log`,
+        timestamp: this.timeStamp,
+        level: 'error'
+      })
+    ]
+  });
+  logger.error(errorMessage);
 }
 
 ContentScraper.prototype.createCSVFile = function (data, fields) {
   let csv = json2csv({ data: data, fields: fields });
 
-  fs.writeFile(`./data/${this.scrapeDate}.csv`, csv, function(err) {
+  fs.writeFile(`./data/${this.scrapeDate}.csv`, csv, (err) => {
     if (err) throw err;
-    console.log('File saved');
+    console.log(`'./data/${this.scrapeDate}.csv' file saved.`);
   });
 };
 
@@ -65,14 +90,13 @@ ContentScraper.prototype.visitLinks = function () {
       .find('#content')
       .set({
         "Title": '.shirt-details>h1',
-        "Color": '.shirt-details>h1',
         "Price": '.shirt-details>h1>.price',
         "ImageURL": 'img@src',
       })
       .data((data) => {
         data.Title = /(M|L)(.*)Shirt/.exec(data.Title)[0];
-        data.Color = /[A-z]+\b$/.exec(data.Color)[0];
-        data.URL = this.allShirtsLinks.shirtLink[i];
+        data.ImageURL = `${this.url}${data.ImageURL}`;
+        data.URL = `${this.url}${this.allShirtsLinks.shirtLink[i]}`;
         data.Time = this.scrapeDate;
         this.savedData.push(data);
       })
@@ -83,7 +107,7 @@ ContentScraper.prototype.visitLinks = function () {
       })
       .error((msg) => {
           if (msg.indexOf('404') > -1) {
-              console.error(`Your page: '${this.url + this.allShirtsLinks.shirtLink[i]}' was not found`);
+            this.createLogFile(`Your page: '${this.url + this.allShirtsLinks.shirtLink[i]}' was not found`);
           }
         }
       );
@@ -91,5 +115,5 @@ ContentScraper.prototype.visitLinks = function () {
 }
 
 const contentScraper = new ContentScraper();
-contentScraper.createDataDirectory();
+contentScraper.createDirectory("./data");
 contentScraper.getShirtsLinks('http://www.shirts4mike.com/shirts.php');
